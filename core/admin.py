5b86804dbc09ec12d9cb0013bdc719cae3e9d891
Dima <92907722+KasperovichDima@users.noreply.github.com>
2022-01-28 22:26:20 +0200

    Revert "lesson code added"

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,3 @@
 from django.contrib import admin
 
-from quiz.models import Choice, Exam, Question, Result
-
-admin.site.register(Exam)
-admin.site.register(Question)
-admin.site.register(Choice)
-admin.site.register(Result)
+# Register your models here.
--- core/models.py
+++ core/models.py
@@ -1,9 +1,3 @@
 from django.db import models
 
-
-class BaseModel(models.Model):
-    create_timestamp = models.DateField(auto_now_add=True)
-    update_timestamp = models.DateField(auto_now=True)
-
-    class Meta:
-        abstract = True
+# Create your models here.
